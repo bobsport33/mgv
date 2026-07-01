45b5af33dec1e7ce7783db19f59e04f3821b97a7
Bob Kelly <bob62kelly@gmail.com>
2026-06-30 21:58:45 -0500

    add css variables, font imports, define font styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import localFont from "next/font/local";
+import { Bricolage_Grotesque } from "next/font/google";
+
+const bricolageGrotesque = Bricolage_Grotesque({
+	subsets: ["latin"],
+	variable: "--font-bricolage-grotesque"
+});
+
+const bodoniCyrillic = localFont({
+	src: "../fonts/Bodoni Cyrillic.ttf",
+	variable: "--font-bodoni-cyrillic"
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<main
+			className={`${bricolageGrotesque.variable} ${bodoniCyrillic.variable}`}
+		>
+			<Component {...pageProps} />
+		</main>
+	);
 }
